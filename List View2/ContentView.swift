5258b1ad00e917bc59f166f49beadcd3b1ecfa89
oshiumitsuyoshi <ot104752gmail.com>
2025-02-10 16:13:37 +0900

    Initial Commit

---
+++ List View2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List View2
+//
+//  Created by 鴛海剛 on 2025/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List View2/List_View2App.swift
@@ -0,0 +1,17 @@
+//
+//  List_View2App.swift
+//  List View2
+//
+//  Created by 鴛海剛 on 2025/02/10.
+//
+
+import SwiftUI
+
+@main
+struct List_View2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
